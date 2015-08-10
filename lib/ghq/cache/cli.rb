7485d66f374cccf1f6997305bafc4338a3b821ec
Takashi Kokubun <takashikkbn@gmail.com>
2015-08-10 20:31:14 +0900

    Implement purging

--- lib/ghq/cache/cli.rb
+++ lib/ghq/cache/cli.rb
@@ -13,6 +13,12 @@ module Ghq
         Logger.log(path)
         refresh
       end
+
+      desc 'purge', 'Delete ~/.ghq-cache and ~/.ghq-cache.log'
+      def purge
+        File.rm_f(Logger::LOG_PATH)
+        File.rm_f(Builder::CACHE_PATH)
+      end
     end
   end
 end
