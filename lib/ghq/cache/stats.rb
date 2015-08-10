4007a1d1498ab77c570b7f380a394ab98ccd9d19
Takashi Kokubun <takashikkbn@gmail.com>
2015-08-10 21:49:17 +0900

    Update stats

--- lib/ghq/cache/stats.rb
+++ lib/ghq/cache/stats.rb
@@ -4,23 +4,29 @@ module Ghq
       class << self
         def print_repository_usage
           root = Logger.resume
+          dir  = list_with_depth(1, root).sort_by(&:count).reverse.first
+          count_max = dir.count.to_s.length
           (1..3).each do |depth|
-            print_usage_with_depth(depth, root)
+            print_usage_with_depth(depth, root, count_max)
           end
         end
 
         private
 
-        def print_usage_with_depth(depth, root)
+        def print_usage_with_depth(depth, root, count_max)
+          list = list_with_depth(depth, root).sort_by(&:count).reverse
+          repo_max  = `ghq list`.split("\n").map(&:length).max
+
           name =
             case depth
             when 1 then 'host'
             when 2 then 'user'
             when 3 then 'repository'
             end
-          puts "[#{name}]" + ('-' * (30 - name.length))
-          list_with_depth(depth, root).sort_by(&:count).reverse.first(30).each do |dir|
-            puts "%5d %s" % [dir.count, dir.path_from_root]
+
+          puts "[#{name}]" + ('-' * (count_max + repo_max + 4 - name.length))
+          list.first(30).each_with_index do |dir, index|
+            puts "[%2d] %-#{repo_max}s %#{count_max}d" % [index + 1, dir.path_from_root, dir.count]
           end
           puts
         end
